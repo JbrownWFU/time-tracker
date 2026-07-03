13c84ce5e94d78b5404d79c99596812f6010a865
JbrownWFU <jensonianbrown@gmail.com>
2026-07-02 20:39:27 -0400

    thurs

--- main.go
+++ main.go
@@ -24,88 +24,95 @@ type Entry struct {
 // General Interface for data storage
 type Repository interface {
 	WriteJob(j Job) error
-	GetJob(id int) (Job, error)
-	// Add write entry
-	// Add read entry
+	WriteEntry(e Entry) error
+
+	ReadJob(id int) (Job, error)
+	ReadEntry(id int) (Entry, error)
 }
 
 // Implement interface
 // Debug type - in memory
 type TestRepo struct {
 	// Store jobs
-	store map[int]Job
+	jobStore   map[int]Job
+	entryStore map[int]Entry
 }
 
 // Instantiate DB / internal storage
 func NewTestRepo() *TestRepo {
 	return &TestRepo{
-		store: make(map[int]Job),
+		jobStore: make(map[int]Job),
+		entryStore: make(map[int]Entry),
 	}
 }
 
-// Satisfies Repository 
+// Job management
+
 func (t *TestRepo) WriteJob(j Job) error {
-	// Write job 
-	t.store[j.ID] = j
+	// Write job
+	t.jobStore[j.ID] = j
 	return nil
 }
 
-// Satisfies Repository 
-func (t *TestRepo) GetJob(id int) (Job, error) {
-	job, ok := t.store[id]
-	
+func (t *TestRepo) ReadJob(id int) (Job, error) {
+	job, ok := t.jobStore[id]
+
 	if !ok {
 		return Job{}, fmt.Errorf("No job with id %d found ", id)
 	}
-	
+
 	return job, nil
+}
+
+// Entry management
+
+func (t *TestRepo) WriteEntry(e Entry) error {
+	// Write job
+	t.entryStore[e.ID] = e
+	return nil
+}
+
+func (t *TestRepo) ReadEntry(id int) (Entry, error) {
+	entry, ok := t.entryStore[id]
+
+	if !ok {
+		return Entry{}, fmt.Errorf("No entry with id %d found ", id)
+	}
+
+	return entry, nil
 }
 
 // Test consumer
 // In the real application this could be useful to store a path to the file
 // Will make in .track config in $HOME but still could be useful for other meta data
-type Storage struct {
-	repo Repository
+type Service struct {
+	repo      Repository
 	createdAt string
 }
 
-func (s *Storage) TestWrite(j Job) error {
+func (s *Service) TestWrite(j Job) error {
 	// Write
 	if err := s.repo.WriteJob(j); err != nil {
 		return err
 	}
-	
+
 	t := time.Now().Format(time.RFC3339)
 	fmt.Println("Job written @ ", t)
 	return nil
 }
 
-func (s *Storage) GetJobById(id int) (Job, error) {
-	job, err := s.repo.GetJob(id)
+func (s *Service) GetJobById(id int) (Job, error) {
+	job, err := s.repo.ReadJob(id)
 	if err != nil {
 		return Job{}, err
 	}
-	
+
 	return job, nil
 }
 
-// Operate on interface directly
-func writeJobRepo(r Repository, j Job) error {
-	if err := r.WriteJob(j); err != nil {
-		return err
-	}
-	
-	return nil
+func (s *Service) GetCreatedAt() string {
+	return s.createdAt
 }
-
-func getJobRepo(r Repository, id int) (Job, error) {
-	if job, err := r.GetJob(id); err != nil {
-		return Job{}, err
-	} else {
-		return job, nil
-	}
-}
-
 func main() {
 	job := Job{
 		1,
@@ -113,43 +120,24 @@ func main() {
 		"A test job",
 		"todo",
 	}
-	
+
 	// Instantiate instance of DB / map
 	repo := NewTestRepo()
-	
+
 	t := time.Now().Format(time.RFC3339)
 
-	// Create instance of Storage with test repo
-	storage := Storage{repo: repo, createdAt: t}
+	// Create instance of Service with test repo
+	storage := Service{repo: repo, createdAt: t}
+	fmt.Printf("Service created @ %s\n", storage.GetCreatedAt())
+
 	storage.TestWrite(job)
-	
+
 	gotJob, err := storage.GetJobById(job.ID)
 	if err != nil {
-		fmt.Println("Error in Storage.GetJobById")
+		fmt.Println("Error in Service.GetJobById")
 		fmt.Println(err)
 	}
-	
+
 	fmt.Printf("Job with id %d:\n", job.ID)
 	fmt.Println(gotJob)
-	
-	// Test write on interface directly
-	fmt.Println("By Repo:")
-	
-	if err = writeJobRepo(repo, job); err != nil {
-		fmt.Println("Error writing job")
-		fmt.Println(err)
-		return
-	}
-	
-	fmt.Println("Job writen via repo")
-	
-	job, err = getJobRepo(repo, job.ID)
-	if err != nil {
-		fmt.Println("Error getting job")
-		fmt.Println(err)
-		return
-	}
-	
-	fmt.Println("Job from repo")
-	fmt.Println(job)
 }
