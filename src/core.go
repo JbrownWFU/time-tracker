4200949c24b97ab6098faee788d94124590ef281
san <jensonianbrown@gmail.com>
2026-07-03 20:26:26 -0400

    fri

--- src/core.go
+++ src/core.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	// "os"
+	"os"
+	"slices"
 )
 
 // Core structs
@@ -25,26 +26,34 @@ type Entry struct {
 
 // General Interface for data storage
 type Repository interface {
-	// Write job to storage and return job
+	// Write job to storage and return job with unique ID
 	WriteJob(j Job) (Job, error)
 	// Return job by id
+	// Might remove if not needed
 	GetJob(id int) (Job, error)
+	// Return job by name
+	GetJobByName(name string) (Job, error)
 	// TODO add update job status
+	UpdateJob(id int, status string) (Job, error)
 
 	// Start time
-	OpenEntry(id int) error
-	// Get current open entry on a job given a name
-	// GetOpenEntry(id int) (Entry, error)
-	// Stop time with optional note
-	CloseEntry(id int, note string) error
+	// OpenEntry(id int) error
+	// // Get current open entry on a job given a name
+	// // GetOpenEntry(id int) (Entry, error)
+	// // Stop time with optional note
+	// CloseEntry(id int, note string) error
 }
-
-// Job is a struct
 
 // Internal service API structure
 type Service struct {
 	repo Repository
 }
+
+var validStatuses = []string{
+	"todo",
+	"active",
+	"done",
+}  
 
 // Create a job and write to repo
 func (s *Service) CreateJob(name string, description string, status string) (Job, error) {
@@ -62,24 +71,54 @@ func (s *Service) CreateJob(name string, description string, status string) (Job
 	return job, err
 }
 
-// Internal function to find job by ID
-func (s *Service) resolveJob(id int) (Job, error) {
-	job, err := s.repo.GetJob(id)
+func (s *Service) UpdateJob(name string, status string) (Job, error) {
+	// Check status is valid
+	if !slices.Contains(validStatuses, status) {
+		return Job{}, fmt.Errorf("Invalid status %s: must be one of %q\n", status, &validStatuses)
+	}
+
+	// Get job
+	job, err := s.repo.GetJobByName(name)
 	if err != nil {
 		return Job{}, err
 	}
 	
-	return job, nil
-}
+	// Update
+	updatedJob, err := s.repo.UpdateJob(job.ID, status)
+	if err != nil {
+		return Job{}, err
+	}
 
-// func (s *Service) CreateJob(j *Job) error {
-// 	if err := s.repo.CreateJob()
-// } 
+	return updatedJob, nil
+}
 
 func main() {
 	// Map Repo testing
-	// repo := NewMapRepo()
+	repo := NewTestRepo()
+	
+	service := Service{repo: repo}
+	fmt.Println("Service started")
+	
+	job, err := service.CreateJob(
+		"Bird Job",
+		"A robot bird project",
+		"todo",
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	
+	fmt.Printf("Job: %+v\n", job)
+	
+	// Update job
+	updatedJob, err := service.UpdateJob(job.Name, "active")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
+	fmt.Printf("Job: %+v\n", updatedJob)
 
 	fmt.Println("Done")
 }
